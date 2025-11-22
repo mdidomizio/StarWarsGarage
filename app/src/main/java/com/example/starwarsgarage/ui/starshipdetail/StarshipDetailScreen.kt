1df03a26b54f64e14085517e5544fd9832c8afe7
mdidomizio <miriam.didomizio@googlemail.com>
2025-11-22 16:40:59 +0100

    adding data on PDP

--- app/src/main/java/com/example/starwarsgarage/ui/starshipdetail/StarshipDetailScreen.kt
+++ app/src/main/java/com/example/starwarsgarage/ui/starshipdetail/StarshipDetailScreen.kt
@@ -1,12 +1,18 @@
 package com.example.starwarsgarage.ui.starshipdetail
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -19,7 +25,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.starwarsgarage.data.remote.Starship
 import com.example.starwarsgarage.ui.StarshipsViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -49,15 +57,37 @@ fun StarshipDetailScreen(viewModel: StarshipsViewModel, starshipId: String, navC
                 .padding(innerPadding),
             contentAlignment = Alignment.Center
         ) {
-            starship?.let {
-                Column {
-                    Text(text = "Name: ${it.name}")
-                    Text(text = "Model: ${it.model}")
-                    Text(text = "Manufacturer: ${it.manufacturer}")
-                    Text(text = "Cost: ${it.costInCredits}")
+            starship?.let { starship ->
+                Column(modifier = Modifier.padding(16.dp)) {
+                    StarshipProperty(label = "Name", value = starship.name)
+                    StarshipProperty(label = "Model", value = starship.model)
+                    StarshipProperty(label = "Manufacturer", value = starship.manufacturer)
+                    StarshipProperty(label = "Cost", value = starship.costInCredits)
+                    StarshipProperty(label = "Length", value = starship.length)
+                    StarshipProperty(label = "Max Atmosphering Speed", value = starship.maxAtmospheringSpeed)
+                    StarshipProperty(label = "Crew", value = starship.crew)
+                    StarshipProperty(label = "Passengers", value = starship.passengers)
+                    StarshipProperty(label = "Cargo Capacity", value = starship.cargoCapacity)
+                    StarshipProperty(label = "Consumables", value = starship.consumables)
+                    StarshipProperty(label = "Hyperdrive Rating", value = starship.hyperdriveRating)
+                    StarshipProperty(label = "MGLT", value = starship.mglt)
+                    StarshipProperty(label = "Starship Class", value = starship.starshipClass)
                 }
-            }
-                ?: CircularProgressIndicator()
+            } ?: CircularProgressIndicator()
         }
     }
+}
+
+@Composable
+fun StarshipProperty(label: String, value: String, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = label)
+        Text(text = value)
+    }
+    Divider()
 }
