5d35249def71f18ac684ecac088aa0662185f8f8
mdidomizio <miriam.didomizio@googlemail.com>
2025-11-23 11:45:26 +0100

    improved home

--- app/src/main/java/com/example/starwarsgarage/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/starwarsgarage/ui/home/HomeScreen.kt
@@ -3,13 +3,21 @@ package com.example.starwarsgarage.ui.home
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.starwarsgarage.R
@@ -17,10 +25,29 @@ import com.example.starwarsgarage.R
 @Composable
 fun HomeScreen(navController: NavHostController, modifier: Modifier = Modifier) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-            Text(text = stringResource(id = R.string.home_screen_title), fontSize = 32.sp)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(id = R.string.home_screen_title),
+                fontSize = 32.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = stringResource(id = R.string.home_screen_subtitle),
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(16.dp))
             Button(onClick = { navController.navigate("starships_list") }) {
                 Text(text = stringResource(id = R.string.go_to_catalog_button))
+                Icon(
+                    Icons.Filled.ArrowForward,
+                    contentDescription = stringResource(id = R.string.go_to_catalog_button),
+                    modifier.padding(start = 8.dp),
+                )
             }
         }
     }
