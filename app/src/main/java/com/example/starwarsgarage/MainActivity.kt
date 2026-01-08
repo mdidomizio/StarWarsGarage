8db80faf5d7002bd5d63b8c75a0027d57621203c
mdidomizio <miriam.didomizio@googlemail.com>
2026-01-08 18:53:39 +0100

    created a appNavigation outside of main activity

--- app/src/main/java/com/example/starwarsgarage/MainActivity.kt
+++ app/src/main/java/com/example/starwarsgarage/MainActivity.kt
@@ -14,20 +14,10 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navigation
-import com.example.starwarsgarage.navigation.AppDestinations.CATALOG_SCREEN_ROUTE
-import com.example.starwarsgarage.navigation.AppDestinations.FAVORITES_SCREEN_ROUTE
-import com.example.starwarsgarage.navigation.AppDestinations.HOME_SCREEN_ROUTE
-import com.example.starwarsgarage.navigation.Screen
+import com.example.starwarsgarage.navigation.AppNavigation
 import com.example.starwarsgarage.ui.AppBottomNavigation
 import com.example.starwarsgarage.ui.SharedViewModel
-import com.example.starwarsgarage.ui.home.HomeScreen
-import com.example.starwarsgarage.ui.pdp.StarshipPdpScreen
-import com.example.starwarsgarage.ui.catalog.StarshipsCatalogScreen
-import com.example.starwarsgarage.ui.favourites.StarshipsFavoritesScreen
 import com.example.starwarsgarage.ui.theme.StarWarsGarageTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -55,43 +45,11 @@ class MainActivity : ComponentActivity() {
                     },
                     bottomBar = { AppBottomNavigation(navController = navController) }
                 ) { innerPadding ->
-                    NavHost(
+                    AppNavigation(
                         navController = navController,
-                        startDestination = HOME_SCREEN_ROUTE,
+                        sharedViewModel = sharedViewModel,
                         modifier = Modifier.padding(innerPadding)
-                    ) {
-                        navigation(startDestination = Screen.Home.route, route = HOME_SCREEN_ROUTE) {
-                            composable(Screen.Home.route) {
-                                HomeScreen(
-                                    navController = navController,
-                                    sharedViewModel = sharedViewModel
-                                )
-                            }
-                        }
-                        navigation(startDestination = Screen.Catalog.route, route = CATALOG_SCREEN_ROUTE) {
-                            composable(Screen.Catalog.route) { backStackEntry ->
-                                StarshipsCatalogScreen(
-                                    navController = navController,
-                                    viewModel = hiltViewModel(),
-                                    sharedViewModel = sharedViewModel
-                                )
-                            }
-                            composable(Screen.Pdp.route) {
-                                StarshipPdpScreen(
-                                    navController = navController,
-                                    sharedViewModel = sharedViewModel
-                                )
-                            }
-                        }
-                        navigation(startDestination = Screen.Favorites.route, route = FAVORITES_SCREEN_ROUTE) {
-                            composable(Screen.Favorites.route) { backStackEntry ->
-                                StarshipsFavoritesScreen(
-                                    navController = navController,
-                                    sharedViewModel = sharedViewModel
-                                )
-                            }
-                        }
-                    }
+                    )
                 }
             }
         }
---
+++ app/src/main/java/com/example/starwarsgarage/navigation/AppNavigation.kt
@@ -0,0 +1,57 @@
+package com.example.starwarsgarage.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.example.starwarsgarage.ui.SharedViewModel
+import com.example.starwarsgarage.ui.catalog.StarshipsCatalogScreen
+import com.example.starwarsgarage.ui.favourites.StarshipsFavoritesScreen
+import com.example.starwarsgarage.ui.home.HomeScreen
+import com.example.starwarsgarage.ui.pdp.StarshipPdpScreen
+
+@Composable
+fun AppNavigation(
+    navController: NavHostController,
+    sharedViewModel: SharedViewModel,
+    modifier: Modifier = Modifier,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = AppDestinations.HOME_SCREEN_ROUTE,
+        modifier = modifier
+    ) {
+        navigation(startDestination = Screen.Home.route, route = AppDestinations.HOME_SCREEN_ROUTE) {
+            composable(Screen.Home.route) {
+                HomeScreen(
+                    navController = navController,
+                    sharedViewModel = sharedViewModel
+                )
+            }
+        }
+        navigation(startDestination = Screen.Catalog.route, route = AppDestinations.CATALOG_SCREEN_ROUTE) {
+            composable(Screen.Catalog.route) { backStackEntry ->
+                StarshipsCatalogScreen(
+                    sharedViewModel = sharedViewModel,
+                    navController = navController
+                )
+            }
+            composable(Screen.Pdp.route) {
+                StarshipPdpScreen(
+                    navController = navController,
+                    sharedViewModel = sharedViewModel
+                )
+            }
+        }
+        navigation(startDestination = Screen.Favorites.route, route = AppDestinations.FAVORITES_SCREEN_ROUTE) {
+            composable(Screen.Favorites.route) { backStackEntry ->
+                StarshipsFavoritesScreen(
+                    navController = navController,
+                    sharedViewModel = sharedViewModel
+                )
+            }
+        }
+    }
+}
